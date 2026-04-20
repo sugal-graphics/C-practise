ef5ae2e0a1190661cc4182c73855a65a3fae4b47
Sugal Darlami <sugaldarlami@gmail.com>
2026-04-20 09:47:00 +0545

    Struct in c#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿Console.Write("Enter your name: ");
-string name = Console.ReadLine();
-Console.WriteLine($"Welcome {name}");
+﻿public struct student
+{
+    public string name;
+    public int age;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        student s1;
+        s1.name = "John";
+        s1.age = 20;
+        Console.WriteLine(s1.name);
+    }
+}
